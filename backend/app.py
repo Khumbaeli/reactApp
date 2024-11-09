0e603f0f4552c64e367a3c5bccee7a63c84cee49
Elias Capriles <elicapriles02@gmail.com>
2024-11-09 16:54:39 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 origins = [
     "http://localhost:3000",
-    "https://reactapp-rft2.onrender.com/",
+    "https://reactapp-rft2.onrender.com",
 ]
 
 app.add_middleware(
